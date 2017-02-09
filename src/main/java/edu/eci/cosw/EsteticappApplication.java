b71cd9ec32fbb3db0de6969fb9898127cc8e47e2
oscar-beltran <oscar-u2011@hotmail.com>
2017-02-08 22:22:38 -0500

    Instancia inicial del proyecto

---
+++ src/main/java/edu/eci/cosw/EsteticappApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticappApplication.class, args);
+	}
+}
