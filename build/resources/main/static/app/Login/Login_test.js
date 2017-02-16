bc387708562497b284013fe773b108ce96c3646f
AndresCaicedo95 <2101240@linux108.labinfo.is.escuelaing.edu.co>
2017-02-16 13:06:09 -0500

    Andres

---
+++ build/resources/main/static/app/Login/Login_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.Login module', function() {
+
+  beforeEach(module('myApp.Login'));
+
+  describe('login controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var loginCtrl = $controller('ControladorLogin');
+      expect(loginCtrl).toBeDefined();
+    }));
+
+  });
+});
---
+++ src/main/resources/static/app/Login/Login.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('myApp.Login', ['ngRoute'])
+
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/Login', {
+                    templateUrl: 'Login/Login.html',
+                    controller: 'ControladorLogin'
+                });
+            }])
+
+
+        .controller('ControladorLogin', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
+                $scope.aut = false;
+                $scope.person = [];
+                var authenticate = function (credentials, callback) {
+                    var headers = credentials ? {authorization: "Basic "
+                                + btoa(credentials.username + ":" + credentials.password)
+                    } : {};
+                    
+                    $http.get('user', {headers: headers}).then(successCallback, errorCallback);
+                    function successCallback(data) {
+                        console.log("entra");
+                        console.log(data.data);
+                        console.log(data.data.name);
+                        if (data.data.name) {
+                            $rootScope.authenticated = true;
+                            console.log("pasooooooooo");
+                        } else {
+                            $rootScope.authenticated = false;
+                            console.log("No pasoooooooo");
+                        }
+                        callback && callback();
+                    }
+                    function errorCallback(error) {
+                        console.log("error");
+                        $rootScope.authenticated = false;
+                        callback && callback();
+                    }
+                };
+
+                authenticate();
+                $scope.credentials = {};
+                $scope.login = function () {
+                    console.log("hace algo")
+                    console.log($scope.credentials)
+                    authenticate($scope.credentials, function () {
+                        if ($rootScope.authenticated) {
+                            $location.path("/view2");
+                            $scope.error = false;
+                        } else {
+                            $location.path("/login");
+                            $scope.error = true;
+                        }
+                        console.log("PAsa por aca")
+                    });
+                };
+            }
+        ]);
