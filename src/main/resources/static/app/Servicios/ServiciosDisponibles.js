5dd16582a628edd07bda7f66bbe747ecdcff0a3d
AndresCaicedo95 <oscar.caicedo-b@mail.escuelaing.edu.co>
2017-02-22 07:25:04 -0500

    Arreglos

--- src/main/resources/static/app/Servicios/ServiciosDisponibles.js
+++ src/main/resources/static/app/Servicios/ServiciosDisponibles.js
@@ -10,57 +10,54 @@ angular.module('myApp.ServiciosDisponibles', ['ngRoute', 'ngMaterial'])
             }])
 
 
-        .controller('ControladorServiciosDisponibles', ['$scope', '$rootScope', '$http', '$location','estilistasCategorias','estCat',function ($scope, $rootScope, $http, $location,estilistasCategorias,estCat) {
-               estilistasCategorias.get(function(data){
-                       console.log(data[0]);
-                       $scope.estilistas = data;
-               })
+        .controller('ControladorServiciosDisponibles', ['$scope', '$rootScope', '$http', '$location', 'estilistasCategorias', 'estCat', function ($scope, $rootScope, $http, $location, estilistasCategorias, estCat) {
+                estilistasCategorias.get(function (data) {
+                    console.log(data[0]);
+                    $scope.estilistas = data;
+                })
 
 
-               $scope.professional = function(){
+                $scope.professional = function () {
                     console.log("Profesional");
                     $location.path("/Professional");
-               }
+                }
 
-               //$scope.estilistas = estCat.get({categoria:localStorage.getItem('categoria')});
+                //$scope.estilistas = estCat.get({categoria:localStorage.getItem('categoria')});
 
 
-         }])
+            }])
 
-        .controller('ListCtrl', function($scope, $mdDialog,$location) {
+        .controller('ListCtrl', function ($scope, $mdDialog, $location) {
 
-          //$scope.estilistas = [
-           // { name: 'Janet Perkins', img: "image/peluqueria.jpg", newMessage: true },
+            //$scope.estilistas = [
+            // { name: 'Janet Perkins', img: "image/peluqueria.jpg", newMessage: true },
             //{ name: 'Mary Johnson', img: 'img/100-1.jpeg', newMessage: false },
             //{ name: 'Peter Carlsson', img: 'img/100-2.jpeg', newMessage: false }
-          //];
+            //];
 
 
-          $scope.goToPerson = function(person, event) {
+            $scope.goToPerson = function (person, event) {
 
-         /*$mdDialog.show(
-              $mdDialog.alert()
-                .title('Navigating')
-                .textContent('Inspect ' + person)
-                .ariaLabel('Person inspect demo')
-                .ok('Neat!')
-                .targetEvent(event)
-            );*/
-
-          $scope.goToPerson = function(estilista, event) {
-            $mdDialog.show(
-              $mdDialog.confirm()
-                .title(estilista)
-                .textContent('Reserva ya tu servicio.')
-                .ariaLabel('Lucky day')
-                .ok('RESERVAR!')
-                .cancel('CANCELAR')
-                .targetEvent( $location.path("/Servicios"))
-            );
-
-          };
-
-
+                /*$mdDialog.show(
+                 $mdDialog.alert()
+                 .title('Navigating')
+                 .textContent('Inspect ' + person)
+                 .ariaLabel('Person inspect demo')
+                 .ok('Neat!')
+                 .targetEvent(event)
+                 );*/
+            }
+            $scope.goToPerson = function (estilista, event) {
+                $mdDialog.show(
+                        $mdDialog.confirm()
+                        .title(estilista)
+                        .textContent('Reserva ya tu servicio.')
+                        .ariaLabel('Lucky day')
+                        .ok('RESERVAR!')
+                        .cancel('CANCELAR')
+                        .targetEvent($location.path("/Servicios"))
+                        );
+            };
         });
 
 
