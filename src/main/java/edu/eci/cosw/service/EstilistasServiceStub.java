ef56d746cef329bb2123bef7ba9dc30a724f3e95
oscar-beltran <oscar-u2011@hotmail.com>
2017-03-28 14:43:03 -0500

    Corregido error al inyectar dos clases a una misma dependencia

--- src/main/java/edu/eci/cosw/service/EstilistasServiceStub.java
+++ src/main/java/edu/eci/cosw/service/EstilistasServiceStub.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * Created by SYSTEM on 19/02/2017.
  */
-@Service
+
 public class EstilistasServiceStub implements EstilistaOperaciones{
 
     private List<Estilista> estilistas;
