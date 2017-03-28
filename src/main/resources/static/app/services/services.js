061b5e1f83aafcc74388c89400dfcbf81101d96e
AndresCaicedo95 <oscar.caicedo-b@mail.escuelaing.edu.co>
2017-03-28 11:31:14 -0500

    perfil

--- src/main/resources/static/app/services/services.js
+++ src/main/resources/static/app/services/services.js
@@ -3,6 +3,14 @@
 // Declare app level module which depends on views, and components
 angular.module('services.modulo', ['ngRoute', 'ngResource'])
 
+
+        .factory('usuario', function($resource){
+            return $resource('./user/email/',{email:"@email"},{
+                GET: { method: 'GET'}
+            });
+
+        })
+        
         .factory('categoria', function($resource){
             return $resource('/categorias',{},{
                 GET: { method: 'GET', isArray: true}
@@ -30,7 +38,7 @@ angular.module('services.modulo', ['ngRoute', 'ngResource'])
          })
 
         .factory('registrarService', function($resource){
-            return $resource('./Registrar',
+            return $resource('./user/Registrar',
             {name:"@name",idCard:"@idCard",password:"@password",email:"@mail",Roles_idRole:{idRole:"@idRole",nombre:"@nombre"},photo:"@photo",state:"@state"},{
                 Registrar: { method: 'POST'}
             });
