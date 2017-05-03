6c4074138928f62a4d0901d961b5e1bf23af6876
jean9108 <joana9108@gmail.com>
2017-05-03 09:08:40 -0500

    Perfil usuario listo

--- src/main/java/edu/eci/cosw/controllersAPI/UserControllerApi.java
+++ src/main/java/edu/eci/cosw/controllersAPI/UserControllerApi.java
@@ -86,9 +86,8 @@ public class UserControllerApi {
     @RequestMapping(path = "/email", method = RequestMethod.POST)
     public ResponseEntity<?> getUser(@RequestBody String email){
         try {
-            System.out.println("Llego aqui "+email);
+            System.out.println("Lllegua aquiiiii "+email);
             User user= users.getUsuario(email,"");
-            //System.out.println(user.getRole());
 //            User user= users.getUserByEmail(email.getEmail());
             return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
         } catch (Exception ex) {
